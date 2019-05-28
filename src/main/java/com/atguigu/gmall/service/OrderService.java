ccfc6285d30d0f011f041a93c0dff7b99463b1d2
Administrator <Administrator@DESKTOP-NMBB83D>
2019-05-28 17:43:00 +0800

    修改接口内容

--- src/main/java/com/atguigu/gmall/service/OrderService.java
+++ src/main/java/com/atguigu/gmall/service/OrderService.java
@@ -1,12 +1,16 @@
 package com.atguigu.gmall.service;
 
+import java.util.List;
+
+import com.atguigu.gmall.bean.UserAddress;
+
 public interface OrderService {
     /**
-     * 初始化订单
-     * 
+     * 初始化
      * @param userId
+     * @return
      */
-    public void initOrder(String userId);
-    
+    public List<UserAddress> initOrder(String userId);
+
     public String sayHellow();
 }
